6d3098217c1ef1611775459b9feb4490699a6d7c
Saurabh <saurabhk215@gmail.com>
2021-01-01 14:59:33 +0530

    First Commit

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const rtc = {
+  // For the local client
+  client: null,
+  // For the local audio and video tracks
+  localAudioTrack: null,
+  localVideoTrack: null,
+};
+
+export const options = {
+  // Pass your app ID here
+  appId: "865276d174784d35a2bb069c3e935963",
+  // Pass a token if your project enables the App Certificate
+  token: "006865276d174784d35a2bb069c3e935963IADQb0ANtrJEi7cATDQgr3BBIcB+NutLBeVdnt2hJ/h1xFu2SuEAAAAAEACZqwdIkDrwXwEAAQCQOvBf",
+};
